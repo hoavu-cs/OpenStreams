4c1d1ad0052b7e03c486c2df3708ba04844380af
Hoa Vu <hvutrong@gmail.com>
2022-09-18 12:11:06 -0700

    update

--- F0-test.py
+++ F0-test.py
@@ -5,13 +5,20 @@ from collections import Counter
 import mmh3
 import math
 import string
+import os, psutil
 
-test_input = [''.join(random.choice(string.ascii_uppercase + string.digits) for k in range(20)) for i in range(500000)]
-print(f"true number of distinct elements: {len(set(test_input))}")
+#test_input = [''.join(random.choice(string.ascii_uppercase + string.digits) for k in range(20)) for i in range(500000)]
+#print(f"true number of distinct elements: {len(set(test_input))}")
 
-F0 = sketches.F0_estimate(epsilon = 0.1, C=2)
+process = psutil.Process(os.getpid())
+F0 = sketches.F0_estimate(epsilon = 0.1, delta = 0.1, C=2)
 
-for i in test_input:
-    F0.insert(i)
+
+for i in range(500000):
+#    token=''.join(random.choice(string.ascii_uppercase + string.digits) for k in range(10))
+    token = randrange(3000,4500)
+    F0.insert(str(token))
 
 print(f"Number of distinct elements estimate returned by the sketch {str(F0.estimator())}")
+
+print(process.memory_info().rss)  # in bytes
--- sketches.py
+++ sketches.py
@@ -2,84 +2,75 @@ import mmh3
 import math
 import random
 import statistics
-
+import hashlib
 
 class Misra_Gries:
-
-    # initialize: parameter k controls heavy hitter threshold, i.e., total_count/k
+    # heavy hitter summary for insertion-only streams
     def __init__(self, k = 1):
+        # parameter k controls heavy hitter threshold, i.e., total_count/k
         self.k = k
-        self.counters = {}
+        self.widthounters = {}
 
-    # insert an element to the data structure
     def insert(self, token):
-        # if the element is already in a bucket
-        if token in self.counters:
-            self.counters[token] += 1
+        # insert a token into the data structure
+        if token in self.widthounters:
+            self.widthounters[token] += 1
         else:
-            # if there are still empty bucket(s)
-            if len(self.counters) < self.k:
-                self.counters[token] = 1
+            if len(self.widthounters) < self.k:
+                self.widthounters[token] = 1
             else:
-                for y in list(self.counters):
-                    self.counters[y] -= 1
-                    if self.counters[y] == 0:
-                        del self.counters[y]
+                for y in list(self.widthounters):
+                    self.widthounters[y] -= 1
+                    if self.widthounters[y] == 0:
+                        del self.widthounters[y]
 
-    # return the buckets of top estimate counts
-    # count - stream length/num_buckets <= estimate count <= count
     def top_counters(self, amount):
-
-        sorted_freq = sorted(self.counters, key=self.counters.get, reverse=True)
+        # return the buckets of top estimate counts
+        # count - stream length/num_buckets <= estimate count <= count
+        sorted_freq = sorted(self.widthounters, key=self.widthounters.get, reverse=True)
         sorted_counters = {}
 
         for y in sorted_freq[:amount]:
-            sorted_counters[y] = self.counters[y]
+            sorted_counters[y] = self.widthounters[y]
 
         return sorted_counters
 
 
 
-# estimate the number of distinct elements
 class F0_estimate():
-
-    # initialize
-    # epsilon: relative error, delta: failure probability
-    def __init__(self, epsilon=0.01, delta=0.01, C=5):
-
+    # estimate the number of distinct elements
+    def __init__(self, epsilon=0.01, delta=0.01, C=5,  hash_type = "mmh3"):
+        # epsilon: relative error, delta: failure probability
         # for each hash function h, maintain the smallest hash value H
-        # basic esimate E = (1/H)-1
-        # return the median of r averages of c basic estimators
+        # z = median of depth averages of width smallest hash values
+        # return 1/z - 1
         self.seed = []
-        self.c = C*math.ceil(1/(epsilon*epsilon))
-        self.r = math.ceil(math.log(2,1/delta))
-        self.seed_range = C*C*self.r*self.c
-
+        self.width = C*math.ceil(1/(epsilon*epsilon))
+        self.depth = math.ceil(math.log(2,1/delta))
+        self.seed_range = C*C*self.depth*self.width
+        self.hash_type = hash_type
+        self.max_128_int = pow(2,128)-1
 
         # initialize the seeds for hash functions
-        self.seed = [[random.randrange(1,self.seed_range) for j in range(self.c)] for i in range(self.r)]
-        self.smallest_hash =[[1 for j in range(self.c)] for i in range(self.r)]
+        self.seed = [[random.randrange(1,self.seed_range) for j in range(self.width)] for i in range(self.depth)]
+        self.smallest_hash =[[1 for j in range(self.width)] for i in range(self.depth)]
 
 
-    def hash(self, token, seed):
-        max_32_int = pow(2,32)-1
-        if type(token) == int:
-            return mmh3.hash(token.to_bytes(5, 'big'), seed, signed = False)/max_32_int
-        else:
-            return mmh3.hash(token, seed, signed = False)/max_32_int
+    def _hash(self, token, seed):
+        # compute the hash of a token
+        return mmh3.hash128(token, seed, signed = False)/self.max_128_int
 
 
     def insert(self, token):
-        for i in range(self.r):
-            for j in range(self.c):
-                hash_value = self.hash(token, self.seed[i][j])
+        # insert a token into the sketch. Token must be byte-like objects
+        for i in range(self.depth):
+            for j in range(self.width):
+                hash_value = self._hash(token, self.seed[i][j])
                 if hash_value < self.smallest_hash[i][j]:
                     self.smallest_hash[i][j] = hash_value
 
-
-
-    # return the estimate for the number of distinct elements inserted so far
     def estimator(self):
-        avg = [statistics.mean(self.smallest_hash[i]) for i in range(self.r)]
+        # return the estimate for the number of distinct elements inserted so far
+        avg = [statistics.mean(self.smallest_hash[i]) for i in range(self.depth)]
         median_of_means = statistics.median(avg)
         return math.ceil((1/median_of_means)-1)
